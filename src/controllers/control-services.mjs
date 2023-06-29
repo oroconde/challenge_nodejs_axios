dd4f76178109219c39b3eb5ffd1a8b8d2ff8b48e
Bryan Orozco <bryan.orozco@global.komatsu>
2023-06-29 07:56:17 -0500

    cambios al controlador para API externo

--- src/controllers/control-services.mjs
+++ src/controllers/control-services.mjs
@@ -1,12 +1,27 @@
 import { apiExterno } from "../services/service.mjs";
 
-export const callApi = (req, res) => {
-  const result = apiExterno;
-  result
-    .then((response) => {
-      res.send(JSON.parse(response));
-    })
-    .catch((err) => {
-      throw err;
-    });
+export const callApi = async (req, res) => {
+  try {
+    const datas = req.params;
+    const called = await apiExterno(datas);
+    if (called) {
+      res.status(200).json(called);
+    } else {
+      res.status(404).json({ msg: "datas not found" });
+    }
+  } catch (error) {
+    throw error;
+  }
 };
+
+// export const callApi = (req, res) => {
+//   const result = apiExterno;
+
+//   result
+//     .then((response) => {
+//       res.send(JSON.parse(response));
+//     })
+//     .catch((err) => {
+//       throw err;
+//     });
+// };
